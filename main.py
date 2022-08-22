16c1f24452916068328ddab812b9702c54330e5d
urnz <rafaelsansana25@hotmail.com>
2022-08-22 16:05:59 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import fitz
+import os
+dir_list = os.listdir(os.getcwd())
+for file in dir_list:
+     if '.pdf' in file:
+          with fitz.open(file) as doc:
+               for page in doc:
+                    lines = page.get_text().split('\n')
+                    with open('output.csv', 'w+', encoding="utf-8") as fo:
+                         n=0
+                         txt=''
+                         for line in lines:
+                              if n == 0 or n == 1 or n == 2:
+                                   n+=1
+                              else:
+                                   txt+=(line+'\n')
+                                   n+=1
+                         sp=txt.split(',')      
+                         fo.write('nome, cpf, rs/pv, cargo\n')
+                         n=0
+                         for i in sp:
+                              if n == 0:
+                                   fo.write(i.strip()+', ')
+                                   n+=1
+                              elif n == 1:
+                                   fo.write(i.strip().replace('CPF ', '')+',')
+                              elif n == 2:
+                                   fo.write(i.strip().replace('RS/PV:', '')+',')
+                                   n+=1
+                              elif n == 1:
+                                   fo.write(i.strip()+'\n')
+                                   n=0
+                              
+                              
