13df59e3c03b6a110160539f84bfe0a2af2c663f
urnz <rafaelsansana25@hotmail.com>
2022-08-25 14:22:45 -0300

    changed everytihng

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import os
 import csv
 dir_list = os.listdir(os.getcwd())
 l=['nome', 'cpf', 'rs/pv', 'cargo']
+txt=''
 for file in dir_list:
      if '.pdf' in file:
           print(file)
           with fitz.open(file) as doc:
                for page in doc:
                     lines = page.get_text().replace('\n', '|')
-                    txt=''
                     n=0
                     for i in lines.split('|'):
                          if n<=2:
@@ -21,7 +21,7 @@ for file in dir_list:
                          else:
                               txt+=i+'\n'
                               n+=1
-          with open('output.csv', 'w+', encoding="utf-8") as fo:
-               w = csv.writer(fo, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-               w.writerow(l)
-               fo.write(str(txt))
+with open('output.csv', 'w+', encoding="utf-8") as fo:
+     w = csv.writer(fo, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+     w.writerow(l)
+     fo.write(str(txt))
