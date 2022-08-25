2b61c98b30bed1d6e1b81801f2b0e73c3bcb1091
urnz <rafaelsansana25@hotmail.com>
2022-08-25 15:43:53 -0300

    last version

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 import fitz
-import fitz
 import os
-import csv
 dir_list = os.listdir(os.getcwd())
-l=['nome', 'cpf', 'rs/pv', 'cargo']
-txt=''
+cpfs=[]
+rs=[]
 for file in dir_list:
      if '.pdf' in file:
           print(file)
           with fitz.open(file) as doc:
                for page in doc:
-                    lines = page.get_text().replace('\n', '|')
+                    lines = page.get_text().replace('\n', '|').split(',')
                     n=0
-                    for i in lines.split('|'):
-                         if n<=2:
-                              n+=1
-                         elif n % 2 == 0:
-                              txt+=i
-                              n+=1
-                         else:
-                              txt+=i+'\n'
-                              n+=1
+                    for line in lines:
+                         if 'CPF' in line:
+                              cpfs.append(line.replace('\n','').strip().replace('CPF ',''))
+                         elif 'RS/' in line:
+                              rs.append(line.replace('\n','').strip())
 with open('output.csv', 'w+', encoding="utf-8") as fo:
-     w = csv.writer(fo, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-     w.writerow(l)
-     fo.write(str(txt))
+     print(len(rs))
+     print(len(cpfs))
+     t='RS/PV, CPF\n'
+     n=0
+     for line in rs:
+          if len(line)<=10:
+               t+=str(line.replace('RS/PV:','').replace('|','').strip()+','+cpfs[n].replace('|','')).replace('RS/PV:','').strip()+'\n'
+               n+=1
+          else:
+               s=''
+               nn=0
+               for i in str(line.replace('RS/PV:','').replace('|','').strip()).replace('RS/PV:', '').strip():
+                    if nn<=10:
+                         s+=i
+                         nn+=1
+                    else:
+                         nn+=1
+               t+=str(s.replace('RS/PV:','').replace('|','').strip()+','+cpfs[n].replace('|','')).replace('RS/PV:','').strip()+'\n'
+               n+=1
+     fo.write(t)
